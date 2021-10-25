a8d94dfea7e707e7fa1141c242f966a48813c8a5
Andrey Zhilin <komplayer31@yandex.ru>
2021-10-25 22:55:18 +0300

    Initial Commit

---
+++ business card/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  business card
+//
+//  Created by Андрей on 25.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
